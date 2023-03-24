2a42e2c469ac1e0acff3616b9bd5df414ebe3e8a
Lippiece <github@lippiece.anonaddy.me>
2023-03-24 17:36:46 +0200

    feat: setup records and timestamps atoms

--- src/state/atoms.tsx
+++ src/state/atoms.tsx
@@ -1,11 +1,35 @@
 import { atom } from "jotai"
 
-export const userAtom  = atom( "" )
-export const imageAtom = atom( "" )
-export const timeAtom  = atom( 0 )
+import convertLinkToName from "../logic/convertLinkToName"
 
 interface Characters {
   [ key: string ]: [ number, number ]
 }
 
-export const charactersAtom = atom<Characters>( [] )
+type Timestamps = Map<string, number>
+type Records = Map<string, Timestamps>
+
+const storageUser     = localStorage.getItem( "user" )
+export const userAtom = atom( storageUser || "" )
+
+const storageImage     = localStorage.getItem( "image" )
+export const imageAtom = atom( storageImage || "" )
+
+const storageTime     = localStorage.getItem( "time" )
+export const timeAtom = atom( storageTime || 0 )
+
+const storageCharacters     = ( localStorage.getItem( "characters" ) )
+export const charactersAtom = atom<Characters>(
+  JSON.parse( storageCharacters ) || [] )
+
+const storageRecords     = localStorage.getItem( "records" )
+export const recordsAtom = atom<Records>(
+  JSON.parse( storageRecords ) || new Map() )
+
+export const timestampsAtom = atom( get => {
+  const records             = get( recordsAtom )
+  const image               = get( imageAtom )
+  const thisImageTimestamps = records
+    .get( convertLinkToName( image ) )
+  return thisImageTimestamps || new Map()
+} )
